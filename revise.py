cfacf269fedb77d211c3e327068183974ef0dcd7
SAKIYAMAA <51530267+SAKIYAMAbar@users.noreply.github.com>
2023-10-20 14:22:55 +0900

    Update revise.py

--- revise.py
+++ revise.py
@@ -1,21 +1,26 @@
 import pandas as pd
 
-# Loading Data
-df = pd.read_csv('cit-HepPh.txt', sep='\t', header=None, names=['node1', 'node2'])#変更前
+#Loading Data
+df = pd.read_csv('soc-flickr.txt', sep='\s+', header=None, names=['node1', 'node2'])#'\s+'
 
-# Get all unique nodes
+#Get all unique nodes
 unique_nodes = pd.unique(df[['node1', 'node2']].values.ravel('K'))
 
-# Mapping unique nodes to new IDs
+#Mapping unique nodes to new IDs
 node_to_id = {old: new for new, old in enumerate(unique_nodes)}
 
-# Apply mapping and change to new ID
+# Apply mapping to new ID
 df['node1'] = df['node1'].map(node_to_id)
 df['node2'] = df['node2'].map(node_to_id)
 
+# Remove opposite direction of same edge
+df['min_node'] = df[['node1', 'node2']].min(axis=1)
+df['max_node'] = df[['node1', 'node2']].max(axis=1)
+df = df.drop_duplicates(subset=['min_node', 'max_node'], keep='first')
+df = df.drop(columns=['min_node', 'max_node'])
+
 
 print(df)
-df_edges = df[['node1', 'node2']]
 
-# Write DataFrame to text file (without index and header)
-df_edges.to_csv('cit-HepPh.txt', sep='\t', header=False, index=False)#変更後
+## Write DataFrame to text file (without index and header)
+df.to_csv('flickr.txt', sep='\t', header=False, index=False)
